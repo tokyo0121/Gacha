c7cd3dd9c28474b009a46d24b7761611884604ba
Saki Igeta <p19026is@gm.tsuda.ac.jp>
2021-05-12 13:47:11 +0900

    Initial Commit

---
+++ Gacha/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gacha
+//
+//  Created by 井ケ田　沙紀 on 2021/05/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
